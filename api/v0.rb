3ae3948be320b054bdaad5be46b047d803eb735d
Eduardo Sebastian <esebastian@altiora.es>
2014-02-09 19:58:32 +0100

    Customize 'event' attributes for endpoint response

--- api/v0.rb
+++ api/v0.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 module VLCTechHub
   class API < Grape::API
     class V0 < Grape::API
@@ -8,13 +10,19 @@ module VLCTechHub
         desc 'Retrieve future scheduled events'
         get do
           events = db['events'].find( { sentMail: true, date: { :$gt => Time.now.utc } } )
-          events.to_a
+          events.to_a.map do |event|
+            event["id"] = event["_id"].to_s
+            event["date"] = event["date"].iso8601
+            event.slice("id", "title", "description", "date", "link")
+          end
         end
 
         desc 'Retrieve a specific event'
         get ':id' do
           event = db['events'].find_one( {_id: BSON::ObjectId(params[:id])} )
-          event
+          event["id"] = event["_id"].to_s
+          event["date"] = event["date"].iso8601
+          event.slice("id", "title", "description", "date", "link")
         end
       end
     end
