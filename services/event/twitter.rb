c778e8296fa6dde1fa557db007fbb180f916da6e
hell03610 <eraseunavez@gmail.com>
2016-03-06 19:28:03 +0100

    Make Event::Twitter client parameter optional, as it is used in tests. Fixes #14

--- services/event/twitter.rb
+++ services/event/twitter.rb
@@ -11,7 +11,7 @@ module VLCTechHub
         Twitter.new.tweet(event)
       end
 
-      def initialize(client)
+      def initialize(client = nil)
         @client = client || ::Twitter::REST::Client.new do |config|
           config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
           config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
